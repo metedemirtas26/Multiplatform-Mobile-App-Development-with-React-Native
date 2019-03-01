381cb8933679a02502aaa65826e2f64b7384c210
metedemirtas26 <metedemirtas26@gmail.com>
2019-03-01 16:10:16 +0100

    Delete Favorites and Alert nw

--- components/FavoriteComponent.js
+++ components/FavoriteComponent.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { FlatList, View, Text, Alert } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
+import Swipeout from 'react-native-swipeout';
+import { deleteFavorite } from '../redux/ActionCreators';
 import { baseUrl } from '../shared/baseUrl';
 
 const mapStateToProps = state => {
     return {
-        dishes: state.dishes,
-        favorites: state.favorites
+      dishes: state.dishes,
+      favorites: state.favorites
     }
-}
+  }
+
+const mapDispatchToProps = dispatch => ({
+    deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId))
+})  
 
 class Favorites extends Component {
 
@@ -21,44 +27,72 @@ class Favorites extends Component {
     render() {
 
         const { navigate } = this.props.navigation;
-
-        const renderMenuItem = ({ item, index }) => {
+        
+        const renderMenuItem = ({item, index}) => {
+    
+            const rightButton = [
+                {
+                    text: 'Delete', 
+                    type: 'delete',
+                    onPress: () => {
+                        Alert.alert(
+                            'Delete Favorite?',
+                            'Are you sure you wish to delete the favorite dish ' + item.name + '?',
+                            [
+                                { 
+                                    text: 'Cancel', 
+                                    onPress: () => console.log(item.name + 'Not Deleted'),
+                                    style: ' cancel'
+                                },
+                                {
+                                    text: 'OK',
+                                    onPress: () => this.props.deleteFavorite(item.id)
+                                }
+                            ],
+                            { cancelable: false }
+                        );
+                        
+                    }
+                }
+            ];
 
             return (
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    hideChevron={true}
-                    onPress={() => navigate('Dishdetail', { dishId: item.id })}
-                    leftAvatar={{ source: { uri: baseUrl + item.image } }}
-                />
+                <Swipeout right={rightButton} autoClose={true}>
+                    <ListItem
+                        key={index}
+                        title={item.name}
+                        subtitle={item.description}
+                        hideChevron={true}
+                        onPress={() => navigate('Dishdetail', { dishId: item.id })}
+                        leftAvatar={{ source: {uri: baseUrl + item.image}}}
+                        />
+                </Swipeout>
             );
         };
 
         if (this.props.dishes.isLoading) {
-            return (
+            return(
                 <Loading />
             );
         }
         else if (this.props.dishes.errMess) {
-            return (
-                <View>
+            return(
+                <View>            
                     <Text>{this.props.dishes.errMess}</Text>
-                </View>
+                </View>            
             );
         }
         else {
             return (
-                <FlatList
+                <FlatList 
                     data={this.props.dishes.dishes.filter(dish => this.props.favorites.some(el => el === dish.id))}
                     renderItem={renderMenuItem}
                     keyExtractor={item => item.id.toString()}
-                />
+                    />
             );
         }
     }
 }
 
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
