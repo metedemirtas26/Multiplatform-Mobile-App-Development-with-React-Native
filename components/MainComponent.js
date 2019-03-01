9f563bd636b2d8fbb1946ed1f928de8f18586653
metedemirtas26 <metedemirtas26@gmail.com>
2019-03-01 16:37:57 +0100

    Secure Store nw

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,7 @@ import Menu from './MenuComponent';
 import ContactUs from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
@@ -144,6 +145,23 @@ const FavoritesNavigator = createStackNavigator({
         })
     });
 
+const LoginNavigator = createStackNavigator({
+    Login: { screen: Login }
+}, {
+        navigationOptions: ({ navigation }) => ({
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTitleStyle: {
+                color: "#fff"
+            },
+            headerTintColor: "#fff",
+            headerLeft: <Icon name="menu" size={24}
+                iconStyle={{ color: 'white' }}
+                onPress={() => navigation.toggleDrawer()} />
+        })
+    });
+
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
         <SafeAreaView style={styles.container}
@@ -260,12 +278,27 @@ const MainNavigator = createDrawerNavigator({
                 />
             ),
         }
-    }
-},
+    },
+    Login:
     {
-        drawerBackgroundColor: '#D1C4E9',
-        contentComponent: CustomDrawerContentComponent
-    });
+        screen: LoginNavigator,
+        navigationOptions: {
+            title: 'Login',
+            drawerLabel: 'Login',
+            drawerIcon: ({ tintColor, focused }) => (
+                <Icon
+                    name='sign-in'
+                    type='font-awesome'
+                    size={24}
+                    iconStyle={{ color: tintColor }}
+                />
+            ),
+        }
+    },
+    initialRouteName: 'Home',
+    drawerBackgroundColor: '#D1C4E9',
+    contentComponent: CustomDrawerContentComponent
+});
 
 class Main extends Component {
 
